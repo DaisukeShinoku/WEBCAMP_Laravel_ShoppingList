595969277bdedd48bfd6ad09b79c6d722504fc80
DaisukeShinoku <shinoku.daisuke.ycu@gmail.com>
2022-05-10 20:58:15 +0900

    ログイン画面の表示

---
+++ resources/views/index.blade.php
@@ -0,0 +1,12 @@
+@extends('layout')
+
+@section('title')(ログイン画面)@endsection
+
+@section('contents')
+  <h1>ログイン</h1>
+  <form action="/login" method="post">
+    email:<input name="email"><br>
+    パスワード:<input name="password" type="password"><br>
+    <button>ログインする</button>
+  </form>
+@endsection
